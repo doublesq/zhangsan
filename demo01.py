536426e937e7839f00a2eafb5aa82b3d18754a56
DoubleSq <shuangqingbut@gmail.com>
2021-01-29 13:08:51 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,47 @@
+
+# print("Hello World!")   #字符串
+# print(23333)   #整数
+# print(2.33)  #小数
+# print(True)  #布尔值
+# print(False)  #布尔值
+# print(())  #元组
+# print([])  #数组
+# print({})  #字典
+# """
+# 锄禾日当午
+# 汗滴禾下土
+# """
+
+# print("哈哈哈",2333,2.33)
+# print("哈哈哈"+"嘻嘻嘻")     #字符串的拼接
+# print("哈哈"*10)
+# print(((1+2)*100-99)/2)
+# print(2>3)
+
+# #变量（要用小写字母）、赋值
+# name = "张三"  #把张三这个值赋值给了名字叫name的这个变量
+# print(name)
+
+# a = float(input("请输入："))    #input输入的默认是字符串
+# b = float(input("请输入："))
+# print("input获取的内容：",a+b)
+
+#数据格式的转换
+# print(type(2333))
+
+# a = str(10)
+# print(type(a))
+
+# a = 'sichd'
+# print(len(a))
+
+"""
+练习：通过代码获取两段内容，并计算他们的长度的和。
+"""
+# a = len(input("请输入内容："))
+# b = len(input("请输入内容："))
+# print("输入字段长度为：",a+b)
+
+a = input("请输入内容：")
+b = input("请输入内容：")
+print("两段字符串的长为：",len(a)+len(b))
